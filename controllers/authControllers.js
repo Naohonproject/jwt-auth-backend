193ef4d92a79d93d4f92b179208c71dbc10dd8fd
LE TUAN BAO <LETUANBAO@mbg8.onmicrosoft.com>
2022-10-01 10:00:33 +0700

    validating user register information

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 
 const User = require("../models/User");
+const TemplateError = require("../utils/errorTemplate");
 
 // register controller
 // this we do not need to check user name or email password existed
@@ -9,6 +10,28 @@ const User = require("../models/User");
 // go into catch block , we return it to user with server error
 exports.registerController = async (req, res) => {
   try {
+    // check whether user and email that exist in database or not to response to client error to represent error
+    // message , therefore message is more specific further just server error from mongodb
+    const isExistedUser = await User.findOne({
+      email: req.body.email,
+    });
+    if (isExistedUser) {
+      return res
+        .status(401)
+        .json(
+          new TemplateError(
+            false,
+            "user name or email is already registered an account"
+          )
+        );
+    }
+    // check register password length
+    const isPasswordValid = req.body.password.length > 6;
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json(new TemplateError(false, "Password must be longer than 6 "));
+    }
     //   hash user password before save to db
     const salt = await bcrypt.genSalt(15);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -22,10 +45,11 @@ exports.registerController = async (req, res) => {
 
     //   save created user to db
     const user = await newUser.save();
+
     //   return user info if save successfully
     return res.json(user);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(new TemplateError(false, "Internal Server Error"));
   }
 };
 
@@ -45,6 +69,6 @@ exports.loginController = async (req, res) => {
     }
     return res.status(200).json(user);
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(new TemplateError(false, error.message));
   }
 };
---
+++ utils/errorTemplate.js
@@ -0,0 +1,8 @@
+class TemplateError {
+  constructor(success, message) {
+    this.success = success;
+    this.message = message;
+  }
+}
+
+module.exports = TemplateError;
